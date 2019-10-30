ce11f12ecf11c2d3cf10df1fa3a72eaddeb97931
Patrícia Duarte <patricia.duarte.dds@gmail.com>
2019-10-30 16:35:39 -0300

    Update abertura.py

--- Exemplos figuras/morfologia/abertura.py
+++ Exemplos figuras/morfologia/abertura.py
@@ -7,7 +7,7 @@ img = cv2.imread('manga1.png',0)
 ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
 # Otsu's thresholding
-ret1,th = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#ret1,th = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 kernel = np.ones((5,5),np.uint8)
 opening = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel)
